91a65c05213d26ba5e2ee53d6fae7820a723f86e
Tiago <tiago.miguel.2@hotmail.com>
2019-06-27 15:38:08 +0100

    Navbar fixed

--- resources/js/components/admin/dashboard.vue
+++ resources/js/components/admin/dashboard.vue
@@ -1,42 +1,43 @@
 <template>
-  <div>
-    <div class="container pt-3">
-      <h4 class="welcome-msg">Bem vindo, {{user.name}}</h4>
-      <h3>Sou Admin</h3>
-    </div>
+    <div>
+        <div class="container pt-3">
+            <h4 class="welcome-msg">Bem vindo, {{user.name}}</h4>
+            <h3>Sou Admin</h3>
+        </div>
 
-    <section id="sidebar-menu">
+        <section id="sidebar-menu">
             <a href="#" id="btn-menu" @click.prevent="toggleNav()">
                 <font-awesome-icon icon="bars" size="lg" />
             </a>
             <div class="sidenav" :class="{ active: active}">
-        <router-link class="nav-link" :to="{name: 'editUsers'}">Editar Utilizadores</router-link>
+                <router-link class="nav-link" :to="{name: 'editUsers'}">Editar Utilizadores</router-link>
 
-        <router-link class="nav-link" :to="{name: 'manageSupports'}">Gerir Apoios</router-link>
-        
+                <router-link class="nav-link" :to="{name: 'manageSupports'}">Gerir Apoios</router-link>
+
+            </div>
+        </section>
+        <router-view></router-view>
     </div>
-    </section>
-    <router-view></router-view>
-  </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      active: true
-    };
-  },
-  methods: {
-    toggleNav() {
+    export default {
+        data() {
+            return {
+                active: true
+            };
+        },
+        methods: {
+            toggleNav() {
                 this.active = !this.active;
                 console.log(this.active);
             }
-  },
-  created() {},
-  computed: {
-    user: function() {
-      return this.$store.state.user;
-    }
-  }
-};
+        },
+        created() {},
+        computed: {
+            user: function () {
+                return this.$store.state.user;
+            }
+        }
+    };
+
 </script>
