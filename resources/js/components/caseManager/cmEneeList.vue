3a41c1cc64d747cb953bda5e8536f5969ca8a59f
Bruno <2160808@my.ipleiria.pt>
2020-06-27 20:47:56 +0100

    testing focus with ref property on cmEneeList set-inter component + .ref.refname..focus() on respective switch case

--- resources/js/components/caseManager/cmEneeList.vue
+++ resources/js/components/caseManager/cmEneeList.vue
@@ -348,7 +348,7 @@
             case "newInteraction":
                 this.currentUser = Object.assign({}, row);
                 this.showNewInteraction=true;
-                this.$nextTick(() => this.$refs.netInteraction.$el.focus());
+                this.$nextTick(() => this.$refs.newInteraction.$el.focus());
                 break;
             case "managePlan":
                 this.currentUser = Object.assign({}, row);
