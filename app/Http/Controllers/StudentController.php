04caaee92152ba3f9d2ed329fcb803d6aa0a2010
Bevas <2160808@my.ipleiria.pt>
2020-10-03 05:31:27 +0100

    fix bug subscription form

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -189,10 +189,6 @@ class StudentController extends Controller
             'identificationNumber' => 'required|integer',
             'enruledYear' => 'required|size:4',
             'curricularYear' => 'required|integer|min:0',
-            'responsibleName' => 'string',
-            'responsibleEmail' => 'email',
-            'responsibleKin' => 'string',
-            'responsiblePhone' => 'integer|regex:/[0-9]{9}/',
             'emergencyName' => 'required|string',
             'emergencyPhone' => 'required|integer|regex:/[0-9]{9}/',
             'emergencyEmail' => 'required|email',
@@ -205,6 +201,11 @@ class StudentController extends Controller
             'neeTypeDisease' => 'required_if:neeTypeAnotherDisease,true|string',
             'functionalAnalysis' => '',
         ]);
+//                     'responsibleName' => 'string',
+//                     'responsibleEmail' => 'email',
+//                     'responsibleKin' => 'string',
+//                     'responsiblePhone' => 'integer|regex:/[0-9]{9}/',
+
 
         for ($i = 0; $i < $request->numberPhotos; $i++) {
             $file = Input::file('photo' . $i);
